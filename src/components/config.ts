ae73ce1d35c9b5a085e862c8cd264c9d38b55270
yacine <lentente1958@gmail.com>
2022-12-15 17:04:45 +0100

    deploy with remote api

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,1 +1,3 @@
-export const link='http://192.168.1.132/qcm/';
+//local deploy http://192.168.1.132/qcm/
+// deploy http://keyboard-typing.epizy.com/
+export const link='http://keyboard-typing.epizy.com/';   
