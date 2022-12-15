d0c9ad0da12259df36e7dceff2a8beb8f5c6ffb8
yacine <lentente1958@gmail.com>
2022-12-15 17:05:27 +0100

    deploy with api

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,3 +1,3 @@
 //local deploy http://192.168.1.132/qcm/
 // deploy http://keyboard-typing.epizy.com/
-export const link='http://keyboard-typing.epizy.com/';   
+export const link='http://keyboard-typing.epizy.com/';  
