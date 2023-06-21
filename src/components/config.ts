6dec681a6c73ede993df1e2f08207fa9761ea85a
yacine <lentente1958@gmail.com>
2023-06-21 09:36:43 +0100

    api paths

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,4 +1,4 @@
 //local deploy http://192.168.1.132/qcm/
 // deploy http://keyboard-typing.epizy.com/
 
-export const link='https://kyeboard-typing.000webhostapp.com/';  
+export const link='https://kyeboard-typing.000webhostapp.com/';
